41a951db9029c8f9b667f1420040139caa087634
fananiwafiq <fananiwafiq@gmail.com>
2017-06-06 18:38:17 +0700

    completed

---
+++ app/src/main/java/xir2n15/remidiuts/telkomschid/navigation/MainActivity.java
@@ -0,0 +1,42 @@
+package xir2n15.remidiuts.telkomschid.navigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //nambah iki
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, tabbed.class));
+            }
+        });
+        // end
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, swipe.class));
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, spinner.class));
+            }
+        });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, drawer.class));
+            }
+        });
+    }
+
+}
